3431fd9584aa71c86ba2334e6b604c8ccabc9280
hgi4657 <hgi4657@naver.com>
2024-04-16 14:54:35 +0900

    개인메인카드 JS 수정

--- card.js
+++ card.js
@@ -1,10 +1,8 @@
+// 1번
 document.addEventListener("DOMContentLoaded", function() {
-    // 각 카드 요소를 선택합니다.
     var cards = document.querySelectorAll('.card');
 
-    // 카드 요소를 클릭할 때 실행할 함수를 정의합니다.
     function cardClickHandler(event) {
-        // 클릭된 카드의 하위 요소인 이름 클래스를 가진 요소의 텍스트를 가져옵니다.
         var name = event.currentTarget.querySelector('.name').innerText;
 
         var url;
@@ -30,19 +28,18 @@ document.addEventListener("DOMContentLoaded", function() {
         window.open(url, '_self');
     }
 
-    // 각 카드 요소에 클릭 이벤트를 추가합니다.
+    // 각 카드에 클릭 이벤트 추가
     cards.forEach(function(card) {
         card.addEventListener('click', cardClickHandler);
     });
 });
 
-
+// 2번
 document.addEventListener("DOMContentLoaded", function() {
     var section = document.querySelector('section');
 
     section.addEventListener('click', function(event) {
         if (event.target.classList.contains('card')) {
-            // 클릭된 카드의 하위 요소인 name 클래스를 가진 요소의 텍스트를 가져와서 URL로 사용합니다.
             var name = document.getElementById('JU').id
             var url = 'https://example.com/' + name;
 
