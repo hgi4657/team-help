f703900af68ec3a6fcc859a3882e8203d14981c5
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-04-18 10:09:15 +0900

    정렬

--- comment_section.js
+++ comment_section.js
@@ -1,7 +1,7 @@
 $("#commentgbox").hide();
 
 $("#btn_editor").click(async function () {
-  $("#commentgbox").toggle();
+    $("#commentgbox").toggle();
 });
 
 
@@ -11,18 +11,18 @@ const newComment = document.createElement('div');
 
 // 배경에 랜덤 색상을 선택되는 함수
 function randomColor() {
-    const color_list = ['#8ABDD1', '#8AD1CF', '#8AA7D1', '#EAEAEA' ,'#FFD5C2' ,'#FFCAC2'];
+    const color_list = ['#8ABDD1', '#8AD1CF', '#8AA7D1', '#EAEAEA', '#FFD5C2', '#FFCAC2'];
     return color_list[Math.floor(Math.random() * color_list.length)];
 }
 
 // 새 댓글 등록 함수
-function addComment(name, text) {    
+function addComment(name, text) {
     // 랜덤배경저장
     const backColor = randomColor();
 
     // 댓글 뻐대
-    const commentHtml = 
-    `<div class="box" style="background-color: ${backColor};">
+    const commentHtml =
+        `<div class="box" style="background-color: ${backColor};">
       <div class="nickname">${name}</div>
       <div class="content">${text}</div>
       <button type="button" class="btn UDbtn del" style="background-color: rgb(221, 221, 221);">삭제</button>
@@ -45,7 +45,7 @@ function addComment(name, text) {
 function delComment() {
     const dels = document.querySelectorAll('.del')
 
-    dels.forEach(function(del) {
+    dels.forEach(function (del) {
         del.addEventListener('click', () => {
 
             let del_check = confirm('정말 삭제하시겠습니까?')
@@ -118,7 +118,7 @@ function delComment() {
 
 
 // 등록 버튼 클릭 시 실행
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById('sav').addEventListener('click', () => {
         // 입력 값 가져오기
@@ -126,7 +126,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const text = document.getElementById('inp_txt').value;
         console.log(name)
         console.log(text)
-    
+
         // 내용 입력 확인
         if (name == '' && text == '') {
             alert("닉네임과 내용은 필수입니다")
@@ -139,14 +139,14 @@ document.addEventListener('DOMContentLoaded', function() {
             if (check === true) {
                 // 댓글을 컨테이너에 추가
                 addComment(name, text);
-            
+
                 // 입력 필드 지우기
                 document.getElementById('inp_name').value = '';
                 document.getElementById('inp_txt').value = '';
-    
+
                 // 삭제 버튼 기능 호출
                 delComment()
-                
+
                 // 수정 버튼 기능 호출(미구현)
                 // corComment()
             } else {
